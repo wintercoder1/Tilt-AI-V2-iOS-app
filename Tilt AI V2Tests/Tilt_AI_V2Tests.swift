d6eb21643309eaf739345c8b525ad4ff5e1ce64e
Steve <steve@Mac.local.tld>
2025-08-23 14:31:55 -0400

    include tests

---
+++ Tilt AI V2Tests/Tilt_AI_V2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Tilt_AI_V2Tests.swift
+//  Tilt AI V2Tests
+//
+//  Created by Steve on 8/21/25.
+//
+
+import Testing
+@testable import Tilt_AI_V2
+
+struct Tilt_AI_V2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
